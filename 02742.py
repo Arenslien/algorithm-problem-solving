a77cdb86cc5652d5c7afab8ff14f454622b295bd
Arenslien <arenslien@gmail.com>
2026-01-08 23:44:38 +0900

    2558 - cpp - bronze

--- 02742.py
+++
@@ -1,6 +0,0 @@
-#BaekJoon 02742
-
-N = int(input()) #정수 N 입력
-
-for i in range(N):
-    print(N-i)
---
+++ all_solved_ac/01_bronze/2558.cpp
@@ -0,0 +1,18 @@
+// Authored by: Arenslien
+// BOJ: 2558 - A+B - 2
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int A, B;
+  cin >> A >> B;
+
+  cout << A+B;
+
+  return 0;
+}
