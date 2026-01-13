8b0f0371eb9cbc16f232eedbb9c2b822675d544f
Arenslien <arenslien@gmail.com>
2026-01-13 16:19:15 +0900

    17298 - 문제 세팅

---
+++ BaaarkingDog/0x05_stack/17298.cpp
@@ -0,0 +1,25 @@
+// Authored by: Arenslien
+// BOJ: 17298 - 오큰수
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N;
+  cin >> N;
+
+  for (int i=0; i<N; ++i) {
+
+
+
+
+
+  }
+
+  return 0;
+}
