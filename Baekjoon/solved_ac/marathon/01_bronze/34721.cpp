b4b9287ef93b0a39adacc5517ebb34a8903f25ca
Arenslien <arenslien@gmail.com>
2026-01-17 23:41:20 +0900

    34721 - cpp - marathon - bronze

---
+++ Baekjoon/solved_ac/marathon/01_bronze/34721.cpp
@@ -0,0 +1,20 @@
+// Authored by: Arenslien
+// BOJ: 34721 - 역사를 걸으면 동국이 보이고
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N;
+  cin >> N;
+
+  while (N--) {
+    cout << "I love DGU\n";
+  }
+
+  return 0;
+}
