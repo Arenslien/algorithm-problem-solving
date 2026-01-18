2a4003f0ba311e998eb79446c2a8f7fefddd5d69
Arenslien <arenslien@gmail.com>
2026-01-18 20:21:02 +0900

    32215 - cpp - marathon - bronze

---
+++ Baekjoon/solved_ac/marathon/01_bronze/32215.cpp
@@ -0,0 +1,18 @@
+// Authored by: Arenslien
+// BOJ: 32215 - 코드마스터 2024
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m, k;
+  cin >> n >> m >> k;
+
+  cout << m * k + m;
+
+  return 0;
+}
