cc3498f895d87f4818df5aff9385ec05b6556c9c
Arenslien <arenslien@gmail.com>
2026-01-21 11:10:17 +0900

    15963 - cpp - marathon - bronze5

---
+++ Baekjoon/solved_ac/marathon/01_bronze/15963.cpp
@@ -0,0 +1,18 @@
+// Authored by: Arenslien
+// BOJ: 15963 - CASIO
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  long long N, M;
+  cin >> N >> M;
+
+  cout << (N == M ? 1 : 0);
+
+  return 0;
+}
