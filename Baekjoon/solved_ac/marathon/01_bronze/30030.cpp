2dce55df57e5b25435c1d892b9d7900af7e9426d
Arenslien <arenslien@gmail.com>
2026-01-21 11:16:56 +0900

    30030 - cpp - marathon - bronze5

---
+++ Baekjoon/solved_ac/marathon/01_bronze/30030.cpp
@@ -0,0 +1,18 @@
+// Authored by: Arenslien
+// BOJ: 30030 - 스위트콘 가격 구하기
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int B;
+  cin >> B;
+  
+  cout << (B / 1.1);
+
+  return 0;
+}
