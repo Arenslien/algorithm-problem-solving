058ac8bd48ae803577bdd46bed3b7e7c18de9149
Arenslien <arenslien@gmail.com>
2026-01-26 23:54:21 +0900

    24052 - cpp - all_solved_ac bronze1- 미완

--- Baekjoon/all_solved_ac/01_bronze/24052.cpp
+++ Baekjoon/all_solved_ac/01_bronze/24052.cpp
@@ -14,25 +14,49 @@ int main() {
 
 
   int arr[N] = {};
+  int result[N] = {};
+
   for (int i=0; i<N; ++i) cin >> arr[i];
 
   for (int i=1; i<N; ++i) {
-    for (int j=i-1; j>-1; --j) {
-      if (arr[j] > arr[i]) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        cnt++;
-        arr[j] = arr[i];
+    int j, temp = arr[i];
+
+    for (j=i-1; arr[j] > temp; --j) {
+      arr[j+1] = arr[j];
+      cnt++;
+
+      if (cnt == K) {
+        for (int k=0; k<N; ++k) result[k] = arr[k];
       }
     }
+
+    if (j + 1 != i) {
+      arr[j + 1] = temp;
+      cnt++;
+    }
+
   }
 
-
+  if (cnt < K) cout << -1;
+  else {
+    for (int i=0; i<N; ++i) cout << result[i] << ' ';
+  }
 
   return 0;
 }
 // 0 1 2 3 4
 // ---------
-// 4 5 1 3 2 : 1
-// 4 5 5 3 2 : 2    (temp = 1)
-// 
+// 4 5 1 3 2 : 0
+
+// 4 5 5 3 2 : 1    (temp = 1)
+// 4 4 5 3 2 : 2    (temp = 1)
+// 1 4 5 3 2 : 3    (temp = ?)
+
+// 1 4 5 5 2 : 4    (temp = 3)
+// 1 4 4 5 2 : 5    (temp = 3)
+// 1 3 4 5 2 : 6    (temp = ?)
+
+// 1 3 4 5 5 : 7    (temp = 2)
+// 1 3 4 4 5 : 8    (temp = 2)
+// 1 3 3 4 5 : 9   (temp = 2)
+// 1 2 3 4 5 : 10   (temp = ?)
