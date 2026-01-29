c72614c790d7c4f1a129dcedd9712bf374d4ad9b
Arenslien <arenslien@gmail.com>
2026-01-29 13:43:26 +0900

    23234 - cpp - marathon - b5

---
+++ Baekjoon/solved_ac/marathon/01_bronze/23234.cpp
@@ -0,0 +1,15 @@
+// Authored by: Arenslien
+// BOJ: 23234 - The World Responds
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "The world says hello!";
+
+  return 0;
+}
