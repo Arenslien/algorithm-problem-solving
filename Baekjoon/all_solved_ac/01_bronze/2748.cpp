920ef611d698bbe2691cf737968fc29702130e10
Arenslien <arenslien@gmail.com>
2026-01-31 22:42:13 +0900

    2748 - 최적화

--- Baekjoon/all_solved_ac/01_bronze/2748.cpp
+++ Baekjoon/all_solved_ac/01_bronze/2748.cpp
@@ -5,6 +5,28 @@
 
 using namespace std;
 
+// 첫 번째 풀이 - 정답.
+// int main() {
+//   ios::sync_with_stdio(false);
+//   cin.tie(nullptr);
+
+//   int n;
+//   cin >> n;
+
+//   long long arr[91] = {};
+//   arr[0] = 0;
+//   arr[1] = 1;
+
+//   for (int i=2; i<91; ++i) {
+//     arr[i] = arr[i-1] + arr[i-2];
+//   }
+
+//   cout << arr[n];
+
+//   return 0;
+// }
+
+// 두 번째 풀이.
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -12,15 +34,18 @@ int main() {
   int n;
   cin >> n;
 
-  long long arr[91] = {};
-  arr[0] = 0;
-  arr[1] = 1;
+  long long fn_1 = 0; // F(n-1) 
+  long long fn_2 = 1; // F(n-2)
+  long long fn = 1;   // F(n)
 
-  for (int i=2; i<91; ++i) {
-    arr[i] = arr[i-1] + arr[i-2];
+  for (int i=2; i<=n; ++i) {
+    fn = fn_1 + fn_2;
+    fn_1 = fn_2;
+    fn_2 = fn;
   }
 
-  cout << arr[n];
+  cout << fn;
 
   return 0;
 }
+
