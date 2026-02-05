03bd94232269d55dcd9f2e1c72d4ba2577735850
Arenslien <arenslien@gmail.com>
2026-02-05 19:22:30 +0900

    0x06 queue 구현 연습

---
+++ Baekjoon/BaaarkingDog/0x06_queue/queue_practice.cpp
@@ -0,0 +1,42 @@
+// Authored by: Arenslien
+// Purpose: Practice Queue
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1000005;
+int dat[MAX];
+int head = 0, tail = 0;
+
+void push(int x) {
+    dat[tail++] = x;
+}
+
+void pop() {
+    head++;
+}
+
+int front() {
+    return dat[head];
+}
+
+int back() {
+    return dat[tail - 1];
+}
+
+void test() {
+    push(10); push(20); push(30);
+    cout << front() << '\n'; // 10
+    cout << back() << '\n'; // 30
+    pop(); pop();
+    push(15); push(25);
+    cout << front() << '\n'; // 30
+    cout << back() << '\n'; // 25
+}
+
+int main() {
+    test();    
+
+    return 0;
+}
