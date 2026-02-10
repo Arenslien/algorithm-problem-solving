a83e71d729598525d046f991e8ea8c62967e83ce
Arenslien <arenslien@gmail.com>
2026-02-11 00:07:24 +0900

    4949 - 미완

--- Baekjoon/solved_ac/class2/4949.cpp
+++ Baekjoon/solved_ac/class2/4949.cpp
@@ -2,12 +2,42 @@
 // BOJ: 4949 - 균형잡힌 세상
 
 #include <iostream>
+#include <string>
+#include <stack>
 
 using namespace std;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+
+  while (true) {
+    // 1. 문자열 입력
+    string line;
+    getline(cin, line);
+
+    // 2. 종료 조건
+    if (line == ".") break;
+
+    stack<char> ST;
+
+    // 3. 스택 괄호쌍 체크: '(', ')', '[', ']'
+    for (const char c : line) {
+      if (c == '(' || c == '[') ST.push(c);
+      else if (c == ')') {
+        if (ST.empty()) ST.push(c);
+        else if (ST.top() == '(') ST.pop();
+
+      }
+      else if (c == ']') {
+        if (ST.empty()) ST.push(c);
+        else if (ST.top() == '[') ST.pop();
+
+      }
+    }
+
+    cout << (ST.empty() ? "yes":"no") << '\n';
+  }
 
   return 0;
 }
