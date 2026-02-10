79f478fb5cb17b3093db168d476be6cead4bb89b
Arenslien <arenslien@gmail.com>
2026-02-10 23:37:46 +0900

    4949 - 문제 아직 ㅍ

---
+++ Baekjoon/solved_ac/class2/4949.cpp
@@ -0,0 +1,13 @@
+// Authored by: Arenslien
+// BOJ: 4949 - 균형잡힌 세상
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
