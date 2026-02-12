f74142d1e5e4a79798e2da686d97e11a36ab5883
Arenslien <arenslien@gmail.com>
2026-02-12 19:59:58 +0900

    2504 - barkingdog - 0x08 - g5

--- Baekjoon/BaaarkingDog/0x08_bracket_pair/2504.cpp
+++ Baekjoon/BaaarkingDog/0x08_bracket_pair/2504.cpp
@@ -7,12 +7,6 @@
 
 using namespace std;
 
-struct element {
-  int i_val = 0;
-  char c_val = '\0';
-};
-
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -20,88 +14,43 @@ int main() {
   string line;
   cin >> line;
 
-  stack<element> st;
-  bool is_valid = true;
-  int result = 0;
+  int ans = 0, temp = 1;
+  stack<char> st;
 
-  for (const char c : line) {
-    if (c == '(' || c == '[') st.push({0, c}); // 여는 괄호 push
-    
-    else if (c == ')') { // 닫는 소괄호
-      if (st.empty()) { // 비어있는 경우
-        is_valid = false;
-        break;
-      }
-      if (st.top().c_val == '(') { // 여는 소괄호 ()
-        st.pop();
-
-        if (!st.empty() && st.top().i_val != 0) {
-          int top = st.top().i_val;
-          st.pop();
-          st.push({top + 2, '\0'});
-        }
-        else st.push({2, '\0'});
-      } 
-      else if (st.top().i_val != 0) { // 숫자인 경우
-        int temp = st.top().i_val;
-        st.pop();
-
-        if (!st.empty() && st.top().c_val == '(') { // (X)의 경우
-          st.pop();
-
-          if (!st.empty() && st.top().i_val != 0) { // top + temp * 2;
-            int top = st.top().i_val;
-            st.pop();
-            st.push({top + temp * 2, '\0'});
-          } 
-          else st.push({temp * 2, '\0'});
-        }
-      }
-      else {
-        is_valid = false;
-        break;
-      }
+  for (int i=0; i<line.size(); ++i) {
+    if (line[i] == '(') {
+      temp *= 2;
+      st.push(line[i]);
     }
-
-    else if (c == ']') { // 닫는 대괄호
-      if (st.empty()) { // 비어있는 경우
-        is_valid = false;
+    else if (line[i] == '[') {
+      temp *= 3;
+      st.push(line[i]);
+    }
+    else if (line[i] == ')') {
+      if (st.empty() || st.top() != '(') {
+        ans = 0;
         break;
       }
-      if (st.top().c_val == '[') { // 여는 소괄호 ()
-        st.pop();
-
-        if (!st.empty() && st.top().i_val != 0) {
-          int top = st.top().i_val;
-          st.pop();
-          st.push({top + 3, '\0'});
-        }
-        else st.push({3, '\0'});
-      } 
-      else if (st.top().i_val != 0) { // 숫자인 경우
-        int temp = st.top().i_val;
-        st.pop();
-
-        if (!st.empty() && st.top().c_val == '[') { // (X)의 경우
-          st.pop();
-
-          if (!st.empty() && st.top().i_val != 0) { // top + temp * 2;
-            int top = st.top().i_val;
-            st.pop();
-            st.push({top + temp * 3, '\0'});
-          } 
-          else st.push({temp * 3, '\0'});
-        }
-      }
-      else {
-        is_valid = false;
+      
+      if (line[i-1] == '(') ans += temp;
+      
+      st.pop();
+      temp /= 2;
+    }
+    else if (line[i] == ']') {
+      if (st.empty() || st.top() != '[') {
+        ans = 0;
         break;
       }
+      
+      if (line[i-1] == '[') ans += temp;
+      
+      st.pop();
+      temp /= 3;
     }
   }
 
-  if (!st.empty() && st.top().i_val != 0) result = st.top().i_val;
-  cout << (is_valid ? result:0);
+  cout << (st.empty() ? ans:0);
 
   return 0;
 }
