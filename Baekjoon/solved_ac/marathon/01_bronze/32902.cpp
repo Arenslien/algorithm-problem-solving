89d1a9d117d4f74669eeb6e43242a513a739b843
Arenslien <arenslien@gmail.com>
2026-02-14 23:33:40 +0900

    32902 - solved_ac - marathon - b4

---
+++ Baekjoon/solved_ac/marathon/01_bronze/32902.cpp
@@ -0,0 +1,15 @@
+// Authored by: Arenslien
+// BOJ: 32902 - Chips
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int k, n;
+  cin >> k >> n;
+
+  cout << n+1 << ' ' << n*k+1;
+
+  return 0;
+}
