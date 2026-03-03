139755c5277c8157aedf428a8ab3728387e7320c
Arenslien <arenslien@gmail.com>
2026-03-03 21:54:27 +0900

    recursion_example - barkingdog - 0x0B - 예제

--- Baekjoon/BaaarkingDog/0x0B_recursion/recursion_example.cpp
+++ Baekjoon/BaaarkingDog/0x0B_recursion/recursion_example.cpp
@@ -16,9 +16,26 @@ int sum_N(int N) {
   return N + sum_N(N-1);
 }
 
+// 0x01 연습 문제 1 - 거듭 제곱: a^b mod m
+int mod(int a, int b, int m) { // 결과값 0: int overflow
+  int num = 1;
+  while (b--) num *= a;
+  return num % m;
+}
+
+using ll = long long;
+ll mod2(ll a, ll b, ll m) {
+  ll num = 1;
+  while (b--) num = num * a % m;
+  return num;
+}
+
+
 int main() {
   print_N(10);
-  cout << '\n' << sum_N(15);
+  cout << '\n' << sum_N(15) << '\n';
 
+  cout << mod(6, 100, 5) << '\n';
+  cout << mod2(6, 100, 5) << '\n';
   return 0;
 }
