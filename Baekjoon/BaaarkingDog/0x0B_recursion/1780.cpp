a0dde9c276594b603e9930d27eeef22ce1f19a07
Arenslien <arenslien@gmail.com>
2026-03-07 19:56:33 +0900

    1780 - barkingdog - 0x0B recursion - s2

--- Baekjoon/BaaarkingDog/0x0B_recursion/1780.cpp
+++ Baekjoon/BaaarkingDog/0x0B_recursion/1780.cpp
@@ -9,30 +9,31 @@ int paper[2187][2187];
 int cnt[3];
 
 void count_paper(int N, int r, int c) {
-  if (N == 1) return;
-  int sum = 0;
-  
-  // 1. 모든 종이 체크
-  for (int i=N*r; i<N*r+N; ++i) {
-    for (int j=N*c; j<N*c+N; ++j) {
-      sum += paper[i][j];
+  // 1. 전체 종이 체크
+  bool is_same = true;
+
+  for (int i=r; i<r+N; ++i) {
+    for (int j=c; j<c+N; ++j) {
+      if (paper[r][c] != paper[i][j]) {
+        is_same = false;
+        break;
+      }
     }
   }
 
-  if (sum == N * N * -1) cnt[0]++;
-  else if (sum == 0) cnt[1]++;
-  else if (sum == N * N) cnt[2]++;
-  else {
-    count_paper(N/3, 0, 0);
-    count_paper(N/3, 0, 1);
-    count_paper(N/3, 0, 2);
-    count_paper(N/3, 1, 0);
-    count_paper(N/3, 1, 1);
-    count_paper(N/3, 1, 2);
-    count_paper(N/3, 2, 0);
-    count_paper(N/3, 2, 1);
-    count_paper(N/3, 2, 2);
+  if (is_same) {
+    cnt[paper[r][c] + 1]++;
+    return;
   }
+  
+  // 2. 9등분 시작
+  int n = N/3;
+  for (int i=0; i<3; ++i) {
+    for (int j=0; j<3; ++j) {
+      count_paper(n, i * n + r, j * n + c);
+    }
+  }
+
 }
 
 int main() {
@@ -41,17 +42,16 @@ int main() {
 
   // 0. N과 종이 값 입력
   int N; cin >> N;
-  for (int i=0; i<N; i++) {
-    for (int j=0; j<N; ++j) {
-      cin >> paper[i][j];
-    }
-  }
+
+  for (int i=0; i<N; ++i)
+  for (int j=0; j<N; ++j)
+    cin >> paper[i][j];
 
   // 1. 재귀
   count_paper(N, 0, 0);
 
   // 2. 결과 출력
-  for (int num : cnt) cout << num << ' ';
+  for (int num : cnt) cout << num << '\n';
 
   return 0;
 }
