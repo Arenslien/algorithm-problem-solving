bac1926701748da839e850cfe8aa88f595712553
Arenslien <arenslien@gmail.com>
2026-03-13 15:38:27 +0900

    9663 - barkingdog - 0x0C backtracking - g4

---
+++ Baekjoon/BaaarkingDog/0x0C_backtracking/9663.cpp
@@ -0,0 +1,65 @@
+// Authored by: Arenslien
+// BOJ: 9663 - N-Queen
+
+#include <iostream>
+
+using namespace std;
+
+int board[15][15];
+int N, cnt = 0;
+
+int dx[4] = {1, -1, 1, -1};
+int dy[4] = {1, 1, -1, -1};
+
+void check(int x, int y, bool check) {
+  for (int i=0; i<N; ++i) {
+    board[x][i] = check;
+    board[i][y] = check;
+  }
+
+  for (int dir=0; dir<4; ++dir) {
+    int nx = x, ny = y;
+    while (true) {
+      nx = nx + dx[dir];
+      ny = ny + dy[dir];
+      cout << "nx, ny: " << nx << ' ' << ny << '\n';
+
+      if (nx < 0 || nx >= N || ny < 0 || ny >= N) break;
+      board[nx][ny] = check;
+    }
+  }
+}
+
+void count_queen(int k) {
+  // 1. Base Condition
+  if (k == N) {
+    cnt++;
+    return;
+  }
+
+  // 2. Backtracking
+  for (int i=0; i<N; ++i) {
+    for (int j=0; j<N; ++j) {
+      if (board[i][j]) continue;
+
+      check(i, j, true); // 
+      count_queen(k+1);
+      check(i, j, false); // 다른 퀸의 영역도 같이 false 해버림.
+    }
+  }
+
+
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> N;
+
+  count_queen(0);
+
+  cout << cnt;
+
+  return 0;
+}
