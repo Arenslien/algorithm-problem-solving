7717701b298d356a7d39d952cd88b09b2e14aa5f
Arenslien <arenslien@gmail.com>
2026-03-15 23:26:20 +0900

    1759 - barkingdog - 0x0C backtracking - g5 - 미완

---
+++ Baekjoon/BaaarkingDog/0x0C_backtracking/1759.cpp
@@ -0,0 +1,55 @@
+// Authored By: Arenslien
+// BOJ: 1759 - 암호 만들기
+
+#include <iostream>
+
+using namespace std;
+
+int L, C;
+
+char code[15];
+int is_used[26];
+
+void solve(int k) {
+  // 1. Base Condition
+  if (k == L) {
+    for (int i=0; i<L; ++i) cout << code[i];
+    cout << '\n';
+    return;
+  }
+
+  // 2. Backtracking
+  for (int i=0; i<26; ++i) {
+    if (is_used[i] == 0) {
+      code[k] = 'a' + i;
+      is_used[i] = 1;
+      solve(k+1);
+      is_used[i] = 0;
+    } 
+  }
+
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> L >> C;
+
+  for (int i=0; i<26; ++i) is_used[i] = -1; 
+
+  for (int i=0; i<C; ++i) {
+    char c; cin >> c;
+    is_used[c - 'a'] = 0;
+  }
+
+  solve(0);
+
+  return 0;
+}
+
+// a b c d e
+// f g h i j
+// k l m n o
+// p q r s t
+// u v w x y z
