bbc14256ea6505b0af59f59c1d0e4cfed12615cc
Arenslien <arenslien@gmail.com>
2026-03-16 14:26:29 +0900

    1182 - barkingdog - 0x0C backtracking - s2

--- Baekjoon/BaaarkingDog/0x0C_backtracking/1182.cpp
+++ Baekjoon/BaaarkingDog/0x0C_backtracking/1182.cpp
@@ -2,32 +2,22 @@
 // BOJ: 1182 - 부분수열의 합
 
 #include <iostream>
-#include <numeric> // accumulate 사용
 
 using namespace std;
 
-int arr[20];
-int sub_arr[20];
-bool is_used[20];
-
 int N, S, cnt = 0;
+int arr[20];
 
-void solve(int k) {
+void solve(int k, int sum) {
   // 1. Base Condition
   if (k == N) {
+    if (sum == S) cnt++;
     return;
   }
 
-  // 2. Backtracking
-  for (int i=0; i<N; ++i) {
-    if (is_used[i]) continue;
-
-    sub_arr[k] = arr[i];
-    is_used[i] = true;
-    if (S == accumulate(sub_arr, sub_arr + k+1, 0)) cnt++;
-    solve(k+1);
-    is_used[i] = false; 
-  }
+  // 2. BackTracking
+  solve(k+1, sum);
+  solve(k+1, sum+arr[k]);
 
   return;
 }
@@ -37,12 +27,61 @@ int main() {
   cin.tie(nullptr);
 
   cin >> N >> S;
-
   for (int i=0; i<N; ++i) cin >> arr[i];
 
-  solve(0);
+  solve(0, 0);
 
+  if (S == 0) cnt--;
   cout << cnt;
 
   return 0;
 }
+
+
+
+
+// #include <iostream>
+// #include <numeric> // accumulate 사용
+
+// using namespace std;
+
+// int arr[20];
+// int sub_arr[20];
+// bool is_used[20];
+
+// int N, S, cnt = 0;
+
+// void solve(int k) {
+//   // 1. Base Condition
+//   if (k == N) {
+//     return;
+//   }
+
+//   // 2. Backtracking
+//   for (int i=0; i<N; ++i) {
+//     if (is_used[i]) continue;
+
+//     sub_arr[k] = arr[i];
+//     is_used[i] = true;
+//     if (S == accumulate(sub_arr, sub_arr + k+1, 0)) cnt++;
+//     solve(k+1);
+//     is_used[i] = false; 
+//   }
+
+//   return;
+// }
+
+// int main() {
+//   ios::sync_with_stdio(false);
+//   cin.tie(nullptr);
+
+//   cin >> N >> S;
+
+//   for (int i=0; i<N; ++i) cin >> arr[i];
+
+//   solve(0);
+
+//   cout << cnt;
+
+//   return 0;
+// }
