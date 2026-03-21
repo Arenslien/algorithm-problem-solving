ad3abd7e9e01ddd8e6a14433f201bbb78fc93638
Arenslien <arenslien@gmail.com>
2026-03-21 09:16:33 +0900

    1003 - solved_ac - class3 DP - s3

--- Baekjoon/solved_ac/class3/1003.cpp
+++ Baekjoon/solved_ac/class3/1003.cpp
@@ -4,41 +4,25 @@
 #include <iostream>
 using namespace std;
 
-int zero_cnt = 0, one_cnt = 0;
+int zero_cnt[41];
+int one_cnt[41];
 int T;
-
-void fibonacci(int n) {
-  // 1. Base Condition
-  if (n == 0) {
-    zero_cnt++;
-    return;
-  }
-  if (n == 1) { 
-    one_cnt++;
-    return;
-  }
-  
-  // 2. Recursion
-  fibonacci(n-1);
-  fibonacci(n-2);
-}
-
-void solve(int n) {
-  zero_cnt = 0, one_cnt = 0;
-
-  fibonacci(n);
-
-  cout << zero_cnt << ' ' << one_cnt << '\n';
-}
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
+  zero_cnt[0] = 1; one_cnt[0] = 0;
+  zero_cnt[1] = 0; one_cnt[1] = 1;
+  for (int i=2; i<=40; ++i) {
+    zero_cnt[i] = zero_cnt[i-2] + zero_cnt[i-1];
+    one_cnt[i] = one_cnt[i-2] + one_cnt[i-1];
+  }
+
   cin >> T;
   while (T--) {
-    int num; cin >> num;
-    solve(num);
+    int n; cin >> n;
+    cout << zero_cnt[n] << ' ' << one_cnt[n] << '\n'; 
   }
 
   return 0;
