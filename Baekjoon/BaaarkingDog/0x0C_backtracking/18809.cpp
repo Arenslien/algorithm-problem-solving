b50cb7e761c1eee6282d9e7b040ab89db7fce0c3
Arenslien <arenslien@gmail.com>
2026-03-24 16:44:45 +0900

    18809 - barkingdog - 0x0C backtracking - g1

--- Baekjoon/BaaarkingDog/0x0C_backtracking/18809.cpp
+++ Baekjoon/BaaarkingDog/0x0C_backtracking/18809.cpp
@@ -24,8 +24,9 @@ const int RED = 2;
 const int FLOWER = 3;
 
 int bfs() {
-  pair<int, int> state[50][50] = {0, }; // X: 배양액 종류, Y: 시간을 저장
+  pair<int, int> state[50][50]; // X: 배양액 종류, Y: 시간을 저장
   queue<pair<int, int>> Q;
+  int cnt = 0;
 
   // 1. 배양액 가능한 땅 체크
   for (int i=0; i<soil.size(); ++i) {
@@ -39,25 +40,37 @@ int bfs() {
   while (!Q.empty()) {
     pair<int, int> cur = Q.front(); Q.pop();
 
+    int cur_state = state[cur.X][cur.Y].X;
+    int cur_time = state[cur.X][cur.Y].Y;
+
+    if (cur_state == FLOWER) continue;
+
     for (int dir=0; dir<4; ++dir) {
       int nx = cur.X + dx[dir];
       int ny = cur.Y + dy[dir];
 
       if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue; // 화단 밖 패스
       if (!board[nx][ny]) continue; // 호수 패스
-      if (state[cur.X][cur.Y] == state[nx][ny]) continue;// 이미 배양액 있는 경우 패스
-      // 배양액 색깔 겹치는지 확인하고 꽃도 여기서 처리해야 함.
-      // 현재 배양액 = G --> R 배양액 만남 같은 초에!
-      // 현재 배양액 = R
-
-      state[nx][ny] = state[cur.X][cur.Y];
-      Q.push({nx, ny});
+      if (state[nx][ny].X == EMPTY) {
+        state[nx][ny] = {cur_state, cur_time + 1};
+        Q.push({nx, ny});
+      }
+      else if (state[nx][ny].X == RED) {
+        if (cur_state == GREEN && cur_time+1 == state[nx][ny].Y) {
+          state[nx][ny].X = FLOWER;
+          cnt++;
+        }
+      }
+      else if (state[nx][ny].X == GREEN) {
+        if (cur_state == RED && cur_time+1 == state[nx][ny].Y) {
+          state[nx][ny].X = FLOWER;
+          cnt++;
+        }
+      }
     }
   }
 
-
-
-
+  return cnt;
 }
 
 
@@ -84,7 +97,7 @@ int main() {
   do {
     int flower = bfs();
     if (mx < flower) mx = flower;
-  } while (next_permutation(brute, brute + G+R)); // {0, 0, 1, 1, 2} // 빈 땅 2, 초록 배양액 2, 빨간 배양액 1 
+  } while (next_permutation(brute, brute + soil.size())); // {0, 0, 1, 1, 2} // 빈 땅 2, 초록 배양액 2, 빨간 배양액 1 
 
   cout << mx;
 
