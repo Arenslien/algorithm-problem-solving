8476428dec2b7180debee3e34b3bda1ec691425e
Arenslien <arenslien@gmail.com>
2026-03-24 15:56:36 +0900

    18809 - barkingdog - 0x0C backtracking - 푸는 중 bfs 설계중

--- Baekjoon/BaaarkingDog/0x0C_backtracking/18809.cpp
+++ Baekjoon/BaaarkingDog/0x0C_backtracking/18809.cpp
@@ -5,20 +5,55 @@
 #include <iostream>
 #include <utility> // pair 사용
 #include <vector>
-
+#include <queue>
 using namespace std;
+
+#define X first
+#define Y second
 
 int N, M, G, R, mx = 0;
 int brute[10]; 
 vector<pair<int, int>> soil; 
+int board[50][50];
 
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
 const int EMPTY = 0;
 const int GREEN = 1;
 const int RED = 2;
 const int FLOWER = 3;
 
 int bfs() {
-  
+  pair<int, int> state[50][50] = {0, }; // X: 배양액 종류, Y: 시간을 저장
+  queue<pair<int, int>> Q;
+
+  // 1. 배양액 가능한 땅 체크
+  for (int i=0; i<soil.size(); ++i) {
+    if (brute[i]) { // G or R 배양액 확정 땅 체크 
+      Q.push({soil[i].X, soil[i].Y}); // 해당 좌표 큐에 추가
+      state[soil[i].X][soil[i].Y] = {brute[i], 0}; // 해당 좌표 배양액 상태 반영 
+    }
+  }
+
+  // 2. BFS
+  while (!Q.empty()) {
+    pair<int, int> cur = Q.front(); Q.pop();
+
+    for (int dir=0; dir<4; ++dir) {
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+
+      if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue; // 화단 밖 패스
+      if (!board[nx][ny]) continue; // 호수 패스
+      if (state[cur.X][cur.Y] == state[nx][ny]) continue;// 이미 배양액 있는 경우 패스
+      // 배양액 색깔 겹치는지 확인하고 꽃도 여기서 처리해야 함.
+      // 현재 배양액 = G --> R 배양액 만남 같은 초에!
+      // 현재 배양액 = R
+
+      state[nx][ny] = state[cur.X][cur.Y];
+      Q.push({nx, ny});
+    }
+  }
 
 
 
