6d2aaf7185343d617b2c133539f5e6ed009980a0
Arenslien <arenslien@gmail.com>
2026-03-27 19:56:03 +0900

    경로 이름 수정 & 18808 문제 코드 작성 중

---
+++ Baekjoon/BaaarkingDog/0x0C_backtracking/18808.cpp
@@ -0,0 +1,18 @@
+// Authored by: Arenslien 
+// BOJ: 18808 - 스티커 붙이기
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> N >> M >> K;
+  while (K--) {
+    
+  }
+
+  return 0;
+}
